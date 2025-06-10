d07a29481a507bcc80a580d2643eb2980bbbd4cb
seblerc <seblerc@example.com>
2025-06-10 22:06:32 +0300

    reCAPTCHA geçici olarak devre dışı bırakıldı

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'haber_sitesi',
-  port: 3307
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-module.exports = db;
+connection.connect((err) => {
+  if (err) {
+    console.error('Aiven bağlantı hatası:', err);
+    return;
+  }
+  console.log('Aiven veritabanına bağlanıldı.');
+});
+
+module.exports = connection;
