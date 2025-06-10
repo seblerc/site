490f3bd293553e40cc642889018853651726f939
seblerc <seblerc@example.com>
2025-06-10 22:16:00 +0300

    reCAPTCHA geçici olarak devre dışı bırakıldı

--- db.js
+++ db.js
@@ -2,12 +2,16 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true // Aiven SSL zorunlu
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-module.exports = pool;
+module.exports = pool.promise(); // Promisify ediliyor!
