d5fb61710a90519bf8bafa01f8473df2e677d9f0
seblerc <seblerc@example.com>
2025-06-12 18:05:43 +0300

    reCAPTCHA geçici olarak devre dışı bırakıldı

--- routes/imageProxy.js
+++ routes/imageProxy.js
@@ -17,7 +17,7 @@ router.get('/:filename', async (req, res) => {
     }
 
     const cloudinaryFilename = rows[0].cloudinary_id;
-    const cloudinaryUrl = `https://res.cloudinary.com/dawwc7cxy/image/upload/v1749667679/haber_gorselleri/${cloudinaryFilename}`;
+const cloudinaryUrl = `https://res.cloudinary.com/dawwc7cxy/image/upload/haber_gorselleri/${cloudinaryFilename}`;
 
     request.get(cloudinaryUrl)
       .on('response', (proxiedRes) => {
